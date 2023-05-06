71b82919b4d5fae5ca7997f7a368f25641d7a9d0
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-05-06 19:54:41 +0300

    Sign Up Method

--- app/src/main/java/com/yusuf/photosharingapp/MainActivity.java
+++ app/src/main/java/com/yusuf/photosharingapp/MainActivity.java
@@ -1,15 +1,24 @@
 package com.yusuf.photosharingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.yusuf.photosharingapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    String emailRegex = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
     ActivityMainBinding binding;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        auth= FirebaseAuth.getInstance();
 
     }
 
@@ -28,6 +38,32 @@ public class MainActivity extends AppCompatActivity {
 
     public void signUp(View view){
 
+        String email = binding.emailText.getText().toString();
+        String password = binding.passwordText.getText().toString();
+
+        if (email.equals("") || password.equals("")){
+            Toast.makeText(this,"E-mail or Password can not be empty.",Toast.LENGTH_LONG).show();
+        }
+        else if (email.matches(emailRegex)){
+            Toast.makeText(MainActivity.this,"Please write your email in the correct format.",Toast.LENGTH_LONG).show();
+        }
+        else {
+            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Toast.makeText(MainActivity.this,"Saved",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this,FeedActivity.class);
+                    startActivity(intent);
+
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                   Toast.makeText(MainActivity.this,e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 
 
