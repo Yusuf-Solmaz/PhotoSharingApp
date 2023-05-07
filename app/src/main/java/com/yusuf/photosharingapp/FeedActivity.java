cc4d2bf044466b525566bc9814e0aa05ffe36124
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-05-07 15:30:04 +0300

    Exceptions

--- app/src/main/java/com/yusuf/photosharingapp/FeedActivity.java
+++ app/src/main/java/com/yusuf/photosharingapp/FeedActivity.java
@@ -1,16 +1,27 @@
 package com.yusuf.photosharingapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.yusuf.photosharingapp.databinding.ActivityFeedBinding;
 import com.yusuf.photosharingapp.databinding.ActivityMainBinding;
+
+import org.checkerframework.checker.units.qual.A;
 
 public class FeedActivity extends AppCompatActivity {
 
     ActivityFeedBinding binding;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +30,45 @@ public class FeedActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-
+        auth= FirebaseAuth.getInstance();
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.feedmenu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.shareImage){
+            Intent intent = new Intent(FeedActivity.this, SharePhotoActivity.class);
+            startActivity(intent);
+        }
+        else if (item.getItemId()==R.id.logOut) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(FeedActivity.this);
+            builder.setTitle("Log Out");
+            builder.setMessage("Are You Sure?");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    auth.signOut();
+                    Intent intent = new Intent(FeedActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            builder.show();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/yusuf/photosharingapp/MainActivity.java
+++ app/src/main/java/com/yusuf/photosharingapp/MainActivity.java
@@ -12,6 +12,8 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
 import com.yusuf.photosharingapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     FirebaseAuth auth;
 
+    String email;
+    String password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,32 +32,54 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        auth= FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance();
+        binding.registerButton.setVisibility(View.GONE);
+        binding.textView3.setVisibility(View.GONE);
+
+        FirebaseUser user = auth.getCurrentUser();
+        if (user !=null){
+            Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
     }
 
 
-    public void signIn(View view){
+    public void signIn(View view) {
+
+        email = binding.emailText.getText().toString();
+        password = binding.passwordText.getText().toString();
+
+       if (emailAndPasswordControl(email,password)){
+           auth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+               @Override
+               public void onSuccess(AuthResult authResult) {
+                   Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+                   startActivity(intent);
+                   finish();
+               }
+           }).addOnFailureListener(new OnFailureListener() {
+               @Override
+               public void onFailure(@NonNull Exception e) {
+                   Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+               }
+           });
+       }
 
     }
 
-    public void signUp(View view){
+    public void signUp(View view) {
 
-        String email = binding.emailText.getText().toString();
-        String password = binding.passwordText.getText().toString();
+        email = binding.emailText.getText().toString();
+        password = binding.passwordText.getText().toString();
 
-        if (email.equals("") || password.equals("")){
-            Toast.makeText(this,"E-mail or Password can not be empty.",Toast.LENGTH_LONG).show();
-        }
-        else if (email.matches(emailRegex)){
-            Toast.makeText(MainActivity.this,"Please write your email in the correct format.",Toast.LENGTH_LONG).show();
-        }
-        else {
-            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+        if (emailAndPasswordControl(email,password)){
+            auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                 @Override
                 public void onSuccess(AuthResult authResult) {
-                    Toast.makeText(MainActivity.this,"Saved",Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(MainActivity.this,FeedActivity.class);
+                    Toast.makeText(MainActivity.this, "Saved", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, FeedActivity.class);
                     startActivity(intent);
 
                     finish();
@@ -60,11 +87,55 @@ public class MainActivity extends AppCompatActivity {
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception e) {
-                   Toast.makeText(MainActivity.this,e.getLocalizedMessage(),Toast.LENGTH_LONG).show();
+                    String errorMessage = "Bir hata oluştu";
+                    if (e instanceof FirebaseAuthException) {
+                        FirebaseAuthException authException = (FirebaseAuthException) e;
+                        switch (authException.getErrorCode()) {
+                            case "ERROR_INVALID_EMAIL":
+                                errorMessage = "Invalid e-mail.";
+                                break;
+                            case "ERROR_EMAIL_ALREADY_IN_USE":
+                                errorMessage = "This e-mail already in use.";
+                                break;
+                            case "ERROR_WEAK_PASSWORD":
+                                errorMessage = "Password is weak";
+                                break;
+
+                        }
+                    }
+                    Toast.makeText(MainActivity.this, errorMessage, Toast.LENGTH_LONG).show();
                 }
             });
         }
     }
 
+    public void visibleSignUp(View view) {
+        binding.textView3.setVisibility(View.VISIBLE);
+        binding.textView2.setVisibility(View.GONE);
+        binding.singInButton.setVisibility(View.GONE);
+        binding.registerButton.setVisibility(View.VISIBLE);
+        binding.textView.setText("Have an account?");
 
+    }
+
+    public void visibleSignIn(View view) {
+        binding.textView2.setVisibility(View.VISIBLE);
+        binding.textView3.setVisibility(View.GONE);
+        binding.singInButton.setVisibility(View.VISIBLE);
+        binding.registerButton.setVisibility(View.GONE);
+        binding.textView.setText("Don't have an account?");
+    }
+
+    public boolean emailAndPasswordControl(String email, String password) {
+        if (email.equals("") || password.equals("")) {
+            Toast.makeText(this, "E-mail or Password can not be empty.", Toast.LENGTH_LONG).show();
+            return false;
+        } else if (!email.matches(emailRegex)) {
+            Toast.makeText(MainActivity.this, "Please write your email in the correct format.", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
 }
